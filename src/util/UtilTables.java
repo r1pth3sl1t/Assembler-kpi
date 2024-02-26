e59d4fe8428b80bbbd4c3e79b0ed2170d89866b7
r1pth3sl1t <yaroslavmolchembayev@gmail.com>
2024-02-26 16:42:26 +0200

    Added exception counter

--- src/util/UtilTables.java
+++ src/util/UtilTables.java
@@ -7,8 +7,9 @@ import java.util.Map;
 
 public class UtilTables {
 
+    public static int errors = 0;
     public enum IdentifierType {
-        BYTE, WORD, DWORD, SEGMENT, LABEL
+        BYTE, WORD, DWORD, SEGMENT, LABEL, EQU
     }
     public static Map<String, Identifier> identifiersTable = new HashMap<>();
 
